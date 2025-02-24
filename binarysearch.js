c79914ab65a178037ccf36c940ff5451f1a63927
Tyler <tbos92@gmail.com>
2025-02-24 18:44:58 -0500

    one more wrote practice for binary search with explanation of logic verbally done

--- binarysearch.js
+++ binarysearch.js
@@ -121,4 +121,21 @@ function binarySearchWrote5(nums, target) {
   return -1;
 }
 
+function binarySearchWrote6(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
 
+  while (left <= right) {
+    let mid = left - Math.floor((right + left) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else if (nums[mid] > target) {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+}
