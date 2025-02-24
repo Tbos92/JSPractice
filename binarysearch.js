405af1750efa141583145df39adf1de799009d74
Tyler <tbos92@gmail.com>
2025-02-23 22:04:24 -0500

    Practiced with binary search some more by repreatedly writing it out and talkting through it, explaining every line to myself outloud as if I were explaing to someone else. Learned a decent amount in doing this actually, really happy with having done this

--- binarysearch.js
+++ binarysearch.js
@@ -27,3 +27,39 @@ let result = binarySearch(nums, target);
 console.log(
   result !== -1 ? `Target found at index: ${result}` : "Target not found"
 );
+
+function binarySearchWrote1(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = left + Math.floor((right - left) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else if (nums[mid] > target) right = mid - 1;
+  }
+
+  return -1;
+}
+
+function binarySearchWrote2(nums, target) {
+  left = 0;
+  right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = left + Math.floor((right - left) / 2);
+
+    if (nums[mid] === target) {
+      console.log(mid);
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else if (nums[mid > target]) {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+}
