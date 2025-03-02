ac7590f6133d0e9b316055b41a6688cc0b5ae4d4
Tyler <tbos92@gmail.com>
2025-03-02 16:26:14 -0500

    One last binary search practice with extensive talking through of every line of the functions to really drive home understanding

--- binarysearch.js
+++ binarysearch.js
@@ -147,7 +147,7 @@ function binarySearchWrote7(nums, target) {
   while (left <= right) {
     let mid = left - Math.floor((right + left) / 2);
 
-    if (nums[mid] === 0) {
+    if (nums[mid] === target) {
       return mid;
     } else if (nums[mid] < target) {
       left = mid + 1;
@@ -163,13 +163,51 @@ function binarySearchWrote8(nums, target) {
   let right = nums.length - 1;
 
   while (left <= right) {
-    mid = left - Math.floor((right + left) / 2);
+    let mid = left - Math.floor((right + left) / 2);
 
-    if (nums[mid] === 0) {
+    if (nums[mid] === target) {
       return mid;
     } else if (nums[mid] <= target) {
       left = mid + 1;
     } else if (nums[mid] >= target) {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+}
+
+function binarySearchWrote9(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = left - Math.floor((right + left) / 2);
+
+    if (num[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else if (nums[mid] > target) {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+}
+
+function binarySearchWrote10(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = left - Math.floor((right + left) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else if (nums[mid] > target) {
       right = mid - 1;
     }
   }
